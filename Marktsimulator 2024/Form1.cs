b6a6040c0b47a7b5bc81daf01f0384face2ed462
Tagesmeister <Pascal.Oestrich@outlook.de>
2024-01-21 15:27:32 +0100

    0.0.1

--- Marktsimulator 2024/Form1.cs
+++ Marktsimulator 2024/Form1.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Marktsimulator_2024
 {
     public partial class Form1 : Form
@@ -6,5 +16,25 @@ namespace Marktsimulator_2024
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int employer = 3;
+            int pubularity = 5;
+            int Machies = 3;
+
+            List<int> attributes = new List<int> { employer, pubularity, Machies };
+
+
+            
+
+
+        }
+
     }
 }
---
+++ Marktsimulator 2024/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marktsimulator_2024
+{
+    public partial class Form2 : Form
+    {
+        private List<int> _attributes;
+        public Form2(List<int> attributes)
+        {
+            _attributes = attributes;
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
